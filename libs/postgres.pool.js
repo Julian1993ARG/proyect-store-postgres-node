9a577fcde4992f325600c8b3178fccc31ea391f4
Julian <julianmartinez_93@hotmail.com>
2022-09-06 19:40:36 -0300

    detalles

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -3,14 +3,9 @@ const { Pool } = require('pg');
 const { config } = require('../config/config')
 
 const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword)
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-    const pool = new Pool({
-        host: 'localhost',
-        port: 5432,
-        user: 'julian',
-        password: 'Patri193',
-        database: 'my_store'
-    });
+    const pool = new Pool({ connectionString:URI} );
 
 module.exports = pool
